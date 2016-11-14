b5819f460e12c03f0730032c636851a76ee94d1a
Andrew <andrew@laytipster.co.uk>
2016-11-14 21:05:55 +0000

    Initial check-in of module demoMVC

---
+++ demoMVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace demoMVC.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index(int? page)
+		{
+			var pageItems = ArticleItems();
+			var articleModels = pageItems as IList<demoMVC.Models.ArticleModel> ?? pageItems.ToList();
+			var pager = new Pagination("Home", articleModels.Count, page);
+
+			var viewModel = new demoMVC.Models.HomeModel();
+			{
+				viewModel.Items =
+					articleModels.Skip((int)((pager.CurrentPage - 1) * pager.PageSize)).Take((int)pager.PageSize);
+				viewModel.PaginationInformation = pager;
+			}
+			return View(viewModel);
+		}
+
+		public static IEnumerable<demoMVC.Models.ArticleModel> ArticleItems()
+		{
+			var dummyItems = new List<demoMVC.Models.ArticleModel>();
+			for (int i = 1; i < 17; i++) {
+				dummyItems.Add(new demoMVC.Models.ArticleModel() {ArticleId = i, ArticleBody = "This is article" + i, Title = "Title: " + i });
+			}
+			return dummyItems;
+		}
+	}
+}
---
+++ demoMVC/Models/ArticleModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace demoMVC.Models
+{
+	public class ArticleModel
+	{
+		public int ArticleId { get; set; }
+		public string Title { get; set; }
+		public string ArticleBody { get; set; }
+	}
+}
